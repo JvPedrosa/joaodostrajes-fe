b70e5542a4c8dc8172725b107aa521f0a7f37f9b
Joao Victor Pedrosa <jotavpedrosa@gmail.com>
2024-06-07 10:17:42 -0300

    feat: shopcategory responsive

--- src/pages/ShopCategory/styles.ts
+++ src/pages/ShopCategory/styles.ts
@@ -16,6 +16,20 @@ export const ShopCategoryIndexSort = styled.div`
   p span {
     font-weight: 600;
   }
+
+  @media (max-width: 1280px) {
+    width: 90%;
+    margin: auto;
+
+    button {
+      padding: 5px 10px;
+      font-size: 12px;
+    }
+
+    p {
+      font-size: 12px;
+    }
+  }
 `;
 
 export const ShopCategoryProducts = styled.div`
@@ -24,6 +38,24 @@ export const ShopCategoryProducts = styled.div`
   grid-template-columns: repeat(4, 1fr);
   gap: 16px;
   row-gap: 80px;
+
+  @media (max-width: 1280px) {
+    width: 90%;
+    margin: 16px auto;
+    row-gap: 40px;
+
+    > div {
+      margin: auto;
+    }
+  }
+
+  @media (max-width: 800px) {
+    width: 80%;
+  }
+
+  @media (max-width: 550px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export const ShopCategoryLoadMore = styled.div`
@@ -37,4 +69,18 @@ export const ShopCategoryLoadMore = styled.div`
   color: #787878;
   font-size: 16px;
   font-weight: 500;
+
+  @media (max-width: 1280px) {
+    width: 200px;
+    margin: 100px auto;
+    height: 60px;
+    font-size: 16px;
+  }
+
+  @media (max-width: 800px) {
+    width: 150px;
+    margin: 60px auto;
+    height: 40px;
+    font-size: 13px;
+  }
 `;
